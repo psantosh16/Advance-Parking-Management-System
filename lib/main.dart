a72b0978d0fcc0a668ab902946909234885ffed2
Santosh Phadtare <santoshphadtare1610@gmail.com>
2023-08-08 22:58:21 +0530

    Created LoginPage UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:apms_project/Pages/login_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ,
+      home: LoginPage(),
     );
   }
 }
