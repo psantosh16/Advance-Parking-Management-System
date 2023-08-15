f25b5f518971b5ef0a655c4dff321ee30bea862b
Niranjan Dorage <niranjandorage@gmail.com>
2023-08-15 19:00:35 +0530

    booking slots onclick function

--- lib/Pages/Screens/Booking/bookingpage.dart
+++ lib/Pages/Screens/Booking/bookingpage.dart
@@ -2,23 +2,30 @@ import 'package:apms_project/Theme/color_theme.dart';
 import 'package:flutter/material.dart';
 import 'bookingbody.dart';
 
-class BookingPage extends StatelessWidget {
+class BookingPage extends StatefulWidget {
   const BookingPage({super.key});
+
+  @override
+  State<BookingPage> createState() => _BookingPageState();
+}
+
+class _BookingPageState extends State<BookingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorTheme.whiteTheme,
       appBar: AppBar(
         leading: GestureDetector(
-          onTap: (){
+          onTap: () {
             Navigator.pushReplacementNamed(context, '/screen');
           },
           child: const Icon(Icons.arrow_back_ios),
         ),
-         centerTitle: false,
-          backgroundColor: ColorTheme.blackTheme,
-          automaticallyImplyLeading: false,
-          title: const Text("Book your slot"),),
+        centerTitle: false,
+        backgroundColor: ColorTheme.blackTheme,
+        automaticallyImplyLeading: false,
+        title: const Text("Book your slot"),
+      ),
       body: const Bookingbody(),
     );
   }
