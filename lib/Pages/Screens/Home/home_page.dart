797ee51d106e59012f9fdf512792148bed01282b
Santosh Phadtare <santoshphadtare1610@gmail.com>
2023-08-18 21:23:19 +0530

    Homepage update 1.2

--- lib/Pages/Screens/Home/home_page.dart
+++ lib/Pages/Screens/Home/home_page.dart
@@ -34,123 +34,149 @@ class _HomePageState extends State<HomePage> {
           child: Stack(
         fit: StackFit.expand,
         children: [
-          LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-            return Column(
-              children: [
-                // Upper
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: SizedBox(
-                    height: constraints.maxHeight / 10,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GestureDetector(
-                            onTap: () {
-                              controller.openDrawer();
-                            },
-                            child: const Icon(
-                              Icons.menu_rounded,
-                              color: Colors.white,
-                              size: 40,
-                            )),
-                        const Text(
-                          "Hi, John!",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 30,
-                              color: Colors.white),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-
-                // Bottom
-                Expanded(
-                  child: DraggableScrollableSheet(
-                       minChildSize: 0.8,
-                      initialChildSize: 0.8,
-                      builder: (BuildContext context,
-                          ScrollController scrollController) {
-                        return Container(
-                          decoration: const BoxDecoration(
+          const SizedBox(height: 4),
+          // Upper
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: SizedBox(
+              // height: constraints.maxHeight / 6,
+              width: ResponsiveUtils.screenWidth(context),
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Drawer
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            controller.openDrawer();
+                          },
+                          child: const Icon(
+                            Icons.menu_rounded,
                             color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(30),
-                              topRight: Radius.circular(30),
-                            ),
-                          ),
-                          child:  ListView(
-                            physics: const ClampingScrollPhysics(),
-                            controller: scrollController,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(20.0),
-                                child: Column(
-                                  children: [
-                                    // Drag indicator
-                                    const SizedBox(
-                                      width: 60,
-                                      child: ClipOval(
-                                        child: Divider(
-                                          thickness: 6,
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(height: ResponsiveUtils.screenHeight(context)*0.016),
-                                    // Horizontal ListView with icons
-                                    SizedBox(
-                                      height: ResponsiveUtils.screenHeight(context)*0.154,
-                                      // width: ResponsiveUtils.screenWidth(context)*0.9,
-                                      child: GridView.builder(
-                                        primary: false,
-                                        gridDelegate:  const SliverGridDelegateWithFixedCrossAxisCount(
-                                          crossAxisCount: 3,
-                                        ),
-                                        itemCount: 3, // Total number of items in the grid
-                                        itemBuilder: (context, idx) {
-                                          return Column(
-                                            children: [
-                                              GestureDetector(
-                                                onTap:(){
-                                                  Navigator.pushNamed(context, "/${_category[idx].title}");
-                                                },
-                                                child: Container(
-                                                  width:
-                                                  ResponsiveUtils.screenWidth(context) *
-                                                      0.24,
-                                                  height:
-                                                  ResponsiveUtils.screenHeight(context) *
-                                                      0.10,
-                                                  padding: const EdgeInsets.all(16),
-                                                  child: Image.asset(
-                                                      "assets/images/${_category[idx].link}"),
-                                                ),
-                                              ),
-                                              Text(
-                                                _category[idx].title,
-                                                style: const TextStyle(
-                                                    fontWeight: FontWeight.bold),
-                                              ),
-                                            ],
-                                          );
-                                        },
-                                      ),
-                                    ),
+                            size: 40,
+                          )),
+                      const Text(
+                        "Hi, John!",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 30,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  // Welcome Text
+                  const Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: Text(
+                      "Welcome to Parkiza!",
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
 
-                                  ],
+          const SizedBox(height: 10),
+          // Bottom
+          Container(
+            decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30),
+              topRight: Radius.circular(30),
+            ),
+          ),
+            child: DraggableScrollableSheet(
+                minChildSize: 0.6,
+                initialChildSize: 0.6,
+                builder: (BuildContext context,
+                    ScrollController scrollController) {
+                  return Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                    child: ListView(
+                      physics: const ClampingScrollPhysics(),
+                      controller: scrollController,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            children: [
+                              // Drag indicator
+                              const SizedBox(
+                                width: 60,
+                                child: ClipOval(
+                                  child: Divider(
+                                    thickness: 6,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                  height: ResponsiveUtils.screenHeight(
+                                          context) *
+                                      0.016),
+                              // Horizontal ListView with icons
+                              SizedBox(
+                                height: ResponsiveUtils.screenHeight(
+                                        context) *
+                                    0.154,
+                                child: GridView.builder(
+                                  primary: false,
+                                  gridDelegate:
+                                      const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 3,
+                                  ),
+                                  itemCount:
+                                      3, // Total number of items in the grid
+                                  itemBuilder: (context, idx) {
+                                    return Column(
+                                      children: [
+                                        GestureDetector(
+                                          onTap: () {
+                                            Navigator.pushNamed(context,
+                                                "/${_category[idx].title}");
+                                          },
+                                          child: Container(
+                                            width: ResponsiveUtils
+                                                    .screenWidth(
+                                                        context) *
+                                                0.28,
+                                            height: ResponsiveUtils
+                                                    .screenHeight(
+                                                        context) *
+                                                0.11,
+                                            padding:
+                                                const EdgeInsets.all(16),
+                                            child: Image.asset(
+                                                "assets/images/${_category[idx].link}"),
+                                          ),
+                                        ),
+                                        Text(
+                                          _category[idx].title,
+                                          style: const TextStyle(
+                                              fontWeight:
+                                                  FontWeight.bold),
+                                        ),
+                                      ],
+                                    );
+                                  },
                                 ),
                               ),
                             ],
                           ),
-                        );
-                      }),
-                )
-              ],
-            );
-          })
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+          ),
         ],
       )),
     );
