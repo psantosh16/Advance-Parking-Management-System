5def071f85e9e7ba949348f7b2f27a0d8972e1b3
Santosh Phadtare <santoshphadtare1610@gmail.com>
2023-10-27 13:47:59 +0530

    Fix: Orientation issue causing UI render errors

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ import 'View/auth/register_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter/services.dart' ;
+
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
@@ -27,10 +29,14 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+ 
+ 
   @override
   Widget build(BuildContext context) {
     // Initialized GetMaterialApp
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
     var loginstatus = "";
     if (FirebaseAuth.instance.currentUser == null) {
       loginstatus = "login";
